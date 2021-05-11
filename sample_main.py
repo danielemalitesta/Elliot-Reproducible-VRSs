14748456e781c9701db53e9d269a7270e11b8a47
daniele <d.malitesta@gmail.com>
2021-05-11 11:26:36 +0200

    First commit.

---
+++ sample_main.py
@@ -0,0 +1,9 @@
+from elliot.run import run_experiment
+import argparse
+
+parser = argparse.ArgumentParser(description="Run sample main.")
+parser.add_argument('--type_experiment', type=str, default='baselines')
+parser.add_argument('--dataset', type=str, default='amazon_baby_alexnet')
+args = parser.parse_args()
+
+run_experiment("config_files/{0}_{1}.yml".format(args.type_experiment, args.dataset))
